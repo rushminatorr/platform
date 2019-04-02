1e8512f0e9bb2392ca003433a46ad3bc1b68b297
SergeRadinovich <serge@edgeworx.io>
2019-04-02 13:16:37 +1300

    Move GKE to us-central1-a and randomize GCP resource names

--- deploy/gcp/gke.tf
+++ deploy/gcp/gke.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "gke-cluster" {
-  name               = "ci-cluster"
+  name               = "iofog-gke-${random_id.instance_id.hex}"
   network            = "default"
-  location           = "australia-southeast1"
+  location           = "us-central1-a"
   initial_node_count = 2
 }
