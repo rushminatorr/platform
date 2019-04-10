8309b8be5dd0bf7f33a310c17e9833a05de5e115
SergeRadinovich <serge@edgeworx.io>
2019-04-11 08:56:22 +1200

    Remove echo from `.travis.yml` and decrease timeout for `wait-for-pod.bash`

--- script/wait-for-pod.bash
+++ script/wait-for-pod.bash
@@ -4,7 +4,7 @@ NAMESPACE="$1"
 SELECTOR="$2"
 CTR=0
 STATE=$(kubectl get pods -l $SELECTOR -n $NAMESPACE --no-headers | awk '{print $3}')
-while [ "$CTR" -lt 40 ] && [ "$STATE" != "Running" ] ; do
+while [ "$CTR" -lt 20 ] && [ "$STATE" != "Running" ] ; do
         STATE=$(kubectl get pods -l $SELECTOR -n $NAMESPACE --no-headers | awk '{print $3}')
         echo "Waiting 15 seconds for pods in namespace $NAMESPACE with labels [$SELECTOR] to be ready..."
         sleep 15
