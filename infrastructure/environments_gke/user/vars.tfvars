bbba565103d9065519e49a203bc596eb14358a95
Rashmi Modhwadia <rashmi.modhwadia@gmail.com>
2019-06-12 14:20:41 +0530

    infrastructure automation for in house environment

---
+++ infrastructure/environments_gke/user/vars.tfvars
@@ -0,0 +1,26 @@
+# Dev variables
+project_id          = "focal-freedom-236620"
+environment         = "rushrush" 
+gcp_region          = "us-west1"
+
+# iofog vars
+controller_ip       = "" # Static ip for loadbalancer, eompty is fine.
+# iofog images
+controller_image    = "gcr.io/focal-freedom-236620/controller:1cb99"
+connector_image     = "gcr.io/focal-freedom-236620/connector:1.0.4"
+operator_image      = "gcr.io/focal-freedom-236620/operator:rc-1.0.0"
+kubelet_image       = "gcr.io/focal-freedom-236620/kubelet:rc-1.0.0"
+
+#packet vars used to setup edge nodes in arm or x86
+packet_project_id   = "880125b9-d7b6-43c3-99f5-abd1af3ce879"
+operating_system    = "ubuntu_16_04"
+packet_facility     = ["sjc1", "ewr1"]             
+count_x86           = "0"
+plan_x86            = "c1.small.x86"
+count_arm           = "0"
+plan_arm            = "c2.large.arm"
+
+# used by ansible for agent configuration
+ssh_key             = "~/.ssh/azure"
+agent_version       = "1.0.14.881"
+agent_repo          = "dev"
---
+++ infrastructure/modules/gke/main.tf
@@ -0,0 +1,89 @@
+#############################################################
+# Spin GKE cluster on GCP
+# Store encrypted kubeconfig in storage bucket
+#############################################################
+variable "project_id"           {}
+variable "gke_name"             {}
+variable "gke_region"           {}
+variable "gke_network_name"     {}
+variable "gke_subnetwork"       {}
+variable "service_account"      {}
+
+# Use GKE module to setup a k8s cluster with specified node pool
+module "gke" {
+    source                      = "terraform-google-modules/kubernetes-engine/google"
+    version                     = "2.0.1"
+    project_id                  = "${var.project_id}"
+    name                        = "${var.gke_name}"
+    region                      = "${var.gke_region}"
+    network                     = "${var.gke_network_name}"
+    subnetwork                  = "${var.gke_subnetwork}"
+    service_account             = "${var.service_account}"
+    ip_range_pods               = "${var.gke_network_name}-pods"
+    ip_range_services           = "${var.gke_network_name}-services"
+    kubernetes_dashboard        = true
+
+    node_pools = [
+    {
+        name                    = "${var.gke_name}-node-pool"
+        machine_type            = "n1-standard-1"
+        min_count               = 1
+        max_count               = 20
+        disk_size_gb            = 20
+        disk_type               = "pd-standard"
+        image_type              = "COS"
+        auto_repair             = true
+        auto_upgrade            = true
+        service_account         = "${var.service_account}"
+        preemptible             = false
+        initial_node_count      = 1
+    }]
+
+    node_pools_oauth_scopes = {
+        all = []
+        "${var.gke_name}-node-pool" = [
+            "https://www.googleapis.com/auth/cloud-platform",
+        ]
+    }
+    node_pools_tags = {
+        all = ["${var.gke_name}-node-tags",]
+        "${var.gke_name}-node-pool" = []
+    }
+    node_pools_taints = {
+        all = []
+        "${var.gke_name}-node-pool" = []
+    }
+    node_pools_labels = {
+        all = {
+            cluster = "${var.gke_name}"
+        }
+        "${var.gke_name}-node-pool" = {}
+    }
+    node_pools_metadata = {
+        all = {}
+
+        "${var.gke_name}-node-pool" = {}
+    }
+}
+
+resource "null_resource" "fetch_kubeconfig" {
+    provisioner "local-exec" {
+        command = "gcloud --quiet beta container clusters get-credentials ${module.gke.name} --region ${module.gke.region} --project ${var.project_id}"
+    }
+    depends_on = ["module.gke"]
+}
+
+resource "null_resource" "encrypt_kubeconfig" {
+    provisioner "local-exec" {
+        command = "gcloud kms encrypt --location global --keyring azure-deployment-secrets --key k8s-secrets --plaintext-file ~/.kube/config --ciphertext-file ~/.kube/${module.gke.name}.kubeconfig.encrypted"
+
+        environment = {
+            CLUSTER   = "${module.gke.name}"
+        }
+    }
+    # Store encrypted kubeconfig in storage bucket
+    provisioner "local-exec" {
+        command = "gsutil cp ~/.kube/${module.gke.name}.kubeconfig.encrypted gs://azure-build-secrets"
+    }
+    depends_on = ["null_resource.fetch_kubeconfig"]
+}
