ebd3891bc14061b906e4116c3df10c3d21cab5b6
Rashmi Modhwadia <rashmi.modhwadia@gmail.com>
2019-06-13 13:51:47 +0530

    remove links from output

--- infrastructure/modules/gcp_network/outputs.tf
+++ infrastructure/modules/gcp_network/outputs.tf
@@ -4,11 +4,6 @@ output "network_name" {
     value       = "${module.vpc.network_name}"
 }
 
-output "network_self_link" {
-    description = "The URI of the VPC being created"
-    value       = "${module.vpc.network_self_link}"
-}
-
 output "routes" {
     description = "The routes associated with this VPC"
     value       = "${module.vpc.routes}"
@@ -27,9 +22,4 @@ output "subnets_names" {
 output "subnets_secondary_ranges" {
     description = "The secondary ranges associated with these subnets"
     value       = "${module.vpc.subnets_secondary_ranges}"
-}
-
-output "subnets_self_links" {
-    description = "The self-links of subnets being created"
-    value       = "${module.vpc.subnets_self_links}"
 }
