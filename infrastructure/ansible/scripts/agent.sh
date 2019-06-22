b65edacc9a651e884b86ff7dab6299e42ab0264e
Rashmi Modhwadia <rashmi.modhwadia@gmail.com>
2019-06-22 14:47:10 +0530

    revert dev package name

--- infrastructure/ansible/scripts/agent.sh
+++ infrastructure/ansible/scripts/agent.sh
@@ -323,12 +323,12 @@ do_install_iofog_dev() {
 	case "$lsb_dist" in
 		ubuntu)
 			curl -s https://packagecloud.io/install/repositories/iofog/iofog-agent-snapshots/script.deb.sh$token | $sh_c "bash"
-			$sh_c "apt-get install -y iofog-agent-dev="$version""
+			$sh_c "apt-get install -y iofog-agent="$version""
 			command_status=$?
 			;;
 		fedora|centos)
 			curl -s https://packagecloud.io/install/repositories/iofog/iofog-agent-snapshots/script.rpm.sh$token  | $sh_c "bash"
-			$sh_c "yum install -y iofog-agent-dev-"$version"-1.noarch"
+			$sh_c "yum install -y iofog-agent-"$version"-1.noarch"
 			command_status=$?
 			;;
 		debian|raspbian)
@@ -336,7 +336,7 @@ do_install_iofog_dev() {
 				$sh_c "apt-get install -y -qq net-tools"
 			fi
 			curl -s https://packagecloud.io/install/repositories/iofog/iofog-agent-snapshots/script.deb.sh$token  | $sh_c "bash"
-			$sh_c "apt-get install -y iofog-agent-dev="$version""
+			$sh_c "apt-get install -y iofog-agent="$version""
 			command_status=$?
 			if [ "$lsb_dist" = "raspbian" ] || [ "$(uname -m)" = "armv7l" ] || [ "$(uname -m)" = "aarch64" ] || [ "$(uname -m)" = "armv8" ]; then
 				$sh_c 'sed -i -e "s|<docker_url>.*</docker_url>|<docker_url>tcp://127.0.0.1:2375/</docker_url>|g" /etc/iofog-agent/config.xml'
